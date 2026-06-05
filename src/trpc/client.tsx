d8c712931e0f0ef09594b5990be7faddcb403644
nguyenduycuong110 <nguyenduycuongntb@gmail.com>
2026-06-05 14:21:25 +0700

    15-meetings-form

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,5 +1,4 @@
 'use client';
- 
 // ^-- to make sure we can mount the Provider from a server component
 import type { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -8,9 +7,7 @@ import { createTRPCContext } from '@trpc/tanstack-react-query';
 import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
- 
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
- 
 let browserQueryClient: QueryClient;
 function getQueryClient() {
   if (typeof window === 'undefined') {
@@ -24,15 +21,13 @@ function getQueryClient() {
   if (!browserQueryClient) browserQueryClient = makeQueryClient();
   return browserQueryClient;
 }
- 
 function getUrl() {
   const base = (() => {
     if (typeof window !== 'undefined') return '';
-    return process.env.NEXT_PUBLIC_APP_URL
+    return process.env.NEXT_PUBLIC_APP_URL;
   })();
   return `${base}/api/trpc`;
 }
- 
 export function TRPCReactProvider(
   props: Readonly<{
     children: React.ReactNode;
@@ -43,7 +38,6 @@ export function TRPCReactProvider(
   //       suspend because React will throw away the client on the initial
   //       render if it suspends and there is no boundary
   const queryClient = getQueryClient();
- 
   const [trpcClient] = useState(() =>
     createTRPCClient<AppRouter>({
       links: [
@@ -54,7 +48,6 @@ export function TRPCReactProvider(
       ],
     }),
   );
- 
   return (
     <QueryClientProvider client={queryClient}>
       <TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
