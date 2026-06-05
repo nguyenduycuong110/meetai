c6eab65ef2de07b01c87118f83bbf64b8518f249
nguyenduycuong110 <nguyenduycuongntb@gmail.com>
2026-06-05 15:31:08 +0700

    17-meeting-filters

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -10,7 +10,7 @@ import { LoadingState } from "@/components/loading-state";
 
 import { columns } from "../components/columns";
 import { DataTable } from "@/components/data-table";
-import { DataPagination } from "../components/data-pagination";
+import { DataPagination } from "@/components/data-pagination";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 
 export const AgentsView = () => {
