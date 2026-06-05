9c947b783e490cf609e6a564eecf39e946843b04
nguyenduycuong110 <nguyenduycuongntb@gmail.com>
2026-06-05 14:50:18 +0700

    16-meetings-dttable

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -2,11 +2,13 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { CornerDownRightIcon, VideoIcon } from "lucide-react"
+
 import { Badge } from "@/components/ui/badge"
 import { GeneratedAvatar } from "@/components/generated-avatar"
-import { AgentGetOne } from "../../types"
 
-export const columns: ColumnDef<AgentGetOne>[] = [
+import { AgentsGetMany } from "../../types"
+
+export const columns: ColumnDef<AgentsGetMany[number]>[] = [
   {
     accessorKey: "name",
     header: "Agent Name",
